ddb9a53052f0f213e395945f28a275d7dbdc3b71
osadchik <osadchiik@gmail.com>
2021-12-26 11:52:36 +0200

    init

---
+++ Assets/Scripts/Client.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using UnityEngine;
+
+public class Client : MonoBehaviour
+{
+    public static Client instance;
+    public static int dataBufferSize = 4096;
+
+    public string ip = "127.0.0.1";
+    public int port = 26950;
+    public int myId = 0;
+    public UDP udp;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        udp = new UDP();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public class UDP
+    {
+        public UdpClient socket;
+        public IPEndPoint endPoint;
+
+        public UDP()
+        {
+            endPoint = new IPEndPoint(IPAddress.Parse(instance.ip), instance.port);
+        }
+
+        public void Connect(int localPort)
+        {
+            socket = new UdpClient(localPort);
+
+            socket.Connect(endPoint);
+            socket.BeginReceive(ReceiveCallback, null);
+        }
+
+        private void ReceiveCallback(IAsyncResult result)
+        {
+            try
+            {
+                byte[] data = socket.EndReceive(result, ref endPoint);
+                socket.BeginReceive(ReceiveCallback, null);
+
+                if (data.Length < 4)
+                {
+                    //TODO: disconnect
+                    return;
+                }
+
+                HandleData(data);
+            }
+            catch
+            {
+                //TODO: disconnect
+            }
+        }
+
+        private void HandleData(byte[] data)
+        {
+            using Packet packet = new Packet(data);
+        }
+    }
+}
+
+
---
+++ Assets/Scripts/Packet.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace Assets.Scripts
+{
+    public class Packet
+    {
+        private List<byte> buffer;
+        private byte[] readableBuffer;
+        private int readPos;
+
+        public Packet()
+        {
+            buffer = new List<byte>();
+            readPos = 0;
+        }
+
+        public Packet(int id)
+        {
+            buffer = new List<byte>();
+            readPos = 0;
+
+            Write(id);
+        }
+
+
+    }
+}
