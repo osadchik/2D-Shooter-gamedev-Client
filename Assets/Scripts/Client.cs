4b61631bbf44061ddfa791ebeac82ff69e7f5c0a
osadchik <osadchiik@gmail.com>
2021-12-27 16:22:55 +0200

    Add background field and change player

--- Assets/Scripts/Client.cs
+++ Assets/Scripts/Client.cs
@@ -1,16 +1,15 @@
-﻿using System.Collections;
+﻿using System;
 using System.Collections.Generic;
-using UnityEngine;
 using System.Net;
 using System.Net.Sockets;
-using System;
+using UnityEngine;
 
 public class Client : MonoBehaviour
 {
     public static Client instance;
     public static int dataBufferSize = 4096;
 
-    public string ip = "127.0.0.1";
+    public string ip = "192.168.0.195";
     public int port = 26950;
     public int myId = 0;
     public TcpClient Tcp { get; set; }
